dc63709f5de96841710991dc518426ccd676a7f7
Alberto Ruiz <alberto.ruiz.iii@gmail.com>
2020-04-10 11:04:45 -0500

    schemas added

--- models/thread.js
+++ models/thread.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const threadSchema = new Schema({
-  name: { type: String, required: true },
+  userId: { type: String, required: true },
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  topicName: { type: String, required: true },
+  content: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
